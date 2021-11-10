9e68bd427109683cb5b2fdb1fd02a30595e15847
clark1816 <33646776+clark1816@users.noreply.github.com>
2021-11-10 10:16:48 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,7 @@ auth.set_access_token(config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_S
 api = tweepy.API(auth)
 
 connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
-cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+cursor = (cursor_factory=psycopg2.extras.DictCursor)
 
 option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreetbets', 'stocktwits', 'chart', 'pattern'), 3)
 
